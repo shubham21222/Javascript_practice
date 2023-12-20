92c93049f561cb6a0aa3590e0a282d5ee6003a78
shubham21222 <shubhamraikwar@hotmail.com>
2023-12-20 18:49:06 +0530

    javascript practice

---
+++ 01_Let_const_Var.js
@@ -0,0 +1,17 @@
+
+let Student_name = "shubham"
+const Student_address = "indore"
+var student_number = 7899899989
+
+// const define only one times and its value not changes
+// var not used in js beacuse its have no control over bloack scope and functiona scope,{} => this is a scope
+
+
+Student_name = "Shiv"
+//Student_address = "Bhopal"
+student_number = 2123133
+
+console.log(Student_name);
+console.log(student_number)
+
+console.table([Student_name],[Student_address],[student_number]);
