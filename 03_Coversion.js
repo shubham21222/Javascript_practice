946107b025acec112d7b5d284657ee63ce0b54ba
shubham21222 <shubhamraikwar@hotmail.com>
2023-12-20 19:20:40 +0530

    shubham

---
+++ 03_Coversion.js
@@ -0,0 +1,25 @@
+let score  = "33skc" ;
+
+
+let score_main  = Number(score)
+console.log(score_main)
+// NaN  not a number
+//null zero value
+// undefined nan
+console.log(typeof score_main)
+
+let isLoggedin = ""
+let isLoggedin2 = Boolean(isLoggedin)
+console.log(isLoggedin2)
+// "" false
+// "shubham" true
+
+// 1 => true 0 => false
+
+
+console.log("1"+2+2);
+console.log(1+2+"2")
+
+// + used for conversion
+// prefix or postfix operator
+// my name is shubha
